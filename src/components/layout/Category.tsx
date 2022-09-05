1c389d188afc3cf01ea4c81bd4504595d0e89c87
Nam Hoang <hainam25101997@gmail.com>
2022-09-05 16:21:38 +0700

    save

--- src/components/layout/Category.tsx
+++ src/components/layout/Category.tsx
@@ -10,57 +10,34 @@ function Category() {
     });
   }
   const [categories, setCategories] = useState<ICategory[]>([]);
+  const [count, setCount] = useState<number>(categories.length);
+
   useEffect(() => {
     getDataUserLogin().then(res => {
       setCategories(res.data)
+      setCount(res.data.length)
     })
   }, [])
+
   return (
-      <div className="dropdown-menu megamenu">
-        <div className="megamenu-inner">
-          <div className="row">
-            <div className="col-md-3">
-              <h2 className="megamenu-title">Column 1</h2>
-              <ul className="vertical-menu">
-                <li><a href="#">Example 1</a></li>
-                <li><a href="#">Example 2</a></li>
-                <li><a href="#">Example 3</a></li>
-                <li><a href="#">Example 4</a></li>
-                <li><a href="#">Example 5</a></li>
-              </ul>
-            </div>
-            <div className="col-md-3">
-              <h2 className="megamenu-title">Column 2</h2>
-              <ul className="vertical-menu">
-                <li><a href="#">Example 6</a></li>
-                <li><a href="#">Example 7</a></li>
-                <li><a href="#">Example 8</a></li>
-                <li><a href="#">Example 9</a></li>
-                <li><a href="#">Example 10</a></li>
-              </ul>
-            </div>
-            <div className="col-md-3">
-              <h2 className="megamenu-title">Column 3</h2>
-              <ul className="vertical-menu">
-                <li><a href="#">Example 11</a></li>
-                <li><a href="#">Example 12</a></li>
-                <li><a href="#">Example 13</a></li>
-                <li><a href="#">Example 14</a></li>
-                <li><a href="#">Example 15</a></li>
-              </ul>
-            </div>
-            <div className="col-md-3">
-              <h2 className="megamenu-title">Column 4</h2>
-              <ul className="vertical-menu">
-                <li><a href="#">Example 16</a></li>
-                <li><a href="#">Example 17</a></li>
-                <li><a href="#">Example 18</a></li>
-                <li><a href="#">Example 19</a></li>
-                <li><a href="#">Example 20</a></li>
-              </ul>
-            </div>
-          </div>
+    <div className="dropdown-menu megamenu">
+      <div className="megamenu-inner">
+        <div className="row">
+          {[...Array(7)].map((item, index) => {
+            return (
+              <div className="col-md-2">
+                <ul className="vertical-menu">
+                  {[...Array(count % 6)].map((item, index2) => {
+                    return (
+                      <li><a href="#">{categories[index * (count % 6) + index2].description}</a></li>
+                    )
+                  })}
+                </ul>
+              </div>
+            )
+          })}
         </div>
+      </div>
     </div>
   )
 }
